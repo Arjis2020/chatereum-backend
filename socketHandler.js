9a606feb95eb1474b5614501b6bb8dd8b91c5e30
Arjis2020 <arjis.chakraborty@gmail.com>
2022-01-17 22:47:41 +0530

    added some more socket handling

--- socketHandler.js
+++ socketHandler.js
@@ -4,7 +4,7 @@ const { rooms: Rooms } = require('./models')
 function init(io) {
     global.io = io
     io.on('connection', (socket) => {
-        console.log(`Socket with ${socket.id} joined`)
+        //console.log(`Socket with ${socket.id} joined`)
         socket.on('join-room', async (data, callback) => {
             const { room_code, username } = data
             socket.nickname = username
@@ -22,17 +22,29 @@ function init(io) {
             else {
                 socket.join(`${room_code}`)
                 callback({
-                    status: 'success'
+                    status: 'success',
+                    participants: io.sockets.adapter.rooms.get(`${room_code}`).size
                 })
                 socket.in(`${room_code}`).emit('room-joined', {
-                    username
+                    username,
+                    participants: io.sockets.adapter.rooms.get(`${room_code}`).size
                 })
             }
         })
-        socket.on("disconnecting", (reason) => {
+        socket.on("disconnecting", async (reason) => {
             for (const room of socket.rooms) {
                 if (room !== socket.id) {
-                    socket.to(room).emit("user-disconnected", socket.nickname);
+                    socket.to(room).emit("user-disconnected", {
+                        username: socket.nickname,
+                        participants: io.sockets.adapter.rooms.get(`${room}`).size - 1
+                    })
+                    if (io.sockets.adapter.rooms.get(`${room}`).size - 1 === 0) {
+                        await Rooms.destroy({
+                            where: {
+                                room_code: room
+                            }
+                        })
+                    }
                 }
             }
         })
